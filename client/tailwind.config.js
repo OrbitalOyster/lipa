95cb6eb5708cea797da0dc5d079dfe26f56d3930
OrbitalOyster <orbitaloyster@gmail.com>
2024-10-15 19:59:37 +0300

    Before updgrade

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,12 +1,12 @@
 /** @type {import('tailwindcss').Config} */
+
 export default {
   content: [
     "./index.html",
-    "./src/**/*.{vue,ts}",
+    "./src/**/*.vue",
   ],
   theme: {
     extend: {},
   },
   plugins: [],
 }
-
