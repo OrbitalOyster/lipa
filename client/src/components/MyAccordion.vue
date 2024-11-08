41014a88cccfaca752ff740fcaa810bd97788eb9
OrbitalOyster <orbitaloyster@gmail.com>
2024-11-08 22:27:29 +0300

    Side panel progress

--- client/src/components/MyAccordion.vue
+++ client/src/components/MyAccordion.vue
@@ -8,18 +8,17 @@
 </script>
 
 <template>
-  <ul>
-    <li v-for="slot in slots" :key="slot.title" :class="{grow: slot.title === selected, 'overflow-auto': slot.title === selected}">
+  <ul class="h-full">
+    <!-- <li v-for="slot in slots" :key="slot.title" :class="{grow: slot.title === selected, 'overflow-auto': slot.title === selected}"> -->
+    <li v-for="slot in slots" :key="slot.title" :class="{grow: slot.title === selected}">
       <div class="toggle" @click="selected = selected === slot.title ? '' : slot.title">
         <h1>
           <span class="inline-block w-10"><font-awesome-icon :icon="['fas', slot.icon]" size="xl" /></span>
           {{slot.title}}
         </h1>
       </div>
-      <div class="mt-1" v-if="slot.title === selected">
-        <div class="overflow-auto">
-          <slot :name="slot.title"/>
-        </div>
+      <div class="slot-container overflow-auto h-full" v-if="slot.title === selected">
+        <slot :name="slot.title"/>
       </div>
     </li>
   </ul>
@@ -34,6 +33,7 @@
   }
 
   li {
+    @apply min-h-12 overflow-hidden;
     @apply bg-white;
     @apply outline-none border border-slate-300 rounded;
     @apply drop-shadow;
@@ -41,10 +41,15 @@
   }
 
   .toggle {
+    @apply h-12;
     @apply text-slate-600;
     @apply flex justify-between items-center;
-    @apply sticky top-0 bg-white;
+    /* @apply sticky top-0 bg-white; */
     @apply drop-shadow;
     @apply cursor-pointer select-none p-2;
+  }
+
+  .slot-container {
+    height: calc(100% - 3rem);
   }
 </style>
