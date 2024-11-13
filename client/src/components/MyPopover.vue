e6ad0ca4cdc54d753e28c7d61e066fc6ab6ba166
OrbitalOyster <orbitaloyster@gmail.com>
2024-11-13 17:05:13 +0300

    Popover progress

--- client/src/components/MyPopover.vue
+++ client/src/components/MyPopover.vue
@@ -1,17 +1,64 @@
 <script setup lang="ts">
-import { ref } from 'vue'
+import { arrow, autoPlacement, autoUpdate, hide, offset, useFloating } from '@floating-ui/vue'
+import { computed, ref } from 'vue'
+const target = ref(null),
+  floating = ref(null),
+  arrowRef = ref(null),
+  { floatingStyles, middlewareData } = useFloating(target, floating, {
+    middleware: [offset(16), autoPlacement(), hide(), arrow({ element: arrowRef })],
+    whileElementsMounted: autoUpdate,
+  }),
+  arrowStyle = computed(() => {
+    const side = <string>middlewareData.value.offset?.placement.split('-')[0],
+      rotation = { top: -135, right: -45, bottom: 45, left: 135 }[side] || 0
+    return {
+      transform: `rotate(${rotation.toString()}deg)`,
+      left: middlewareData.value.arrow?.x != null
+        ? `${middlewareData.value.arrow.x.toString()}px`
+        : `${(floating.value?.offsetWidth * (side === 'left') - 9).toString()}px`,
+      top: middlewareData.value.arrow?.y != null
+        ? (`${middlewareData.value.arrow.y.toString()}px`)
+        : `${(floating.value?.offsetHeight * (side === 'top') - 9).toString()}px`,
+    }
+  })
 </script>
 
 <template>
-  <div class="absolute top-0 left-0 px-6 py-3">
-    <slot />
-  </div>
+  <span>
+    <div
+      ref="floating"
+      class="floating"
+      :style="[floatingStyles, { display: middlewareData.hide?.referenceHidden ? 'none' : 'block' }]"
+      @click="middlewareData.hide"
+    >
+      <div
+        ref="arrowRef"
+        class="arrow"
+        :style="arrowStyle"
+      />
+      <slot name="popover" />
+    </div>
+    <div
+      ref="target"
+      class="inline-block"
+    >
+      <slot />
+    </div>
+  </span>
 </template>
 
 <style scoped>
-  div {
+  .floating {
+    @apply absolute top-0 left-0 px-6 py-3;
     @apply border border-slate-300 rounded;
     @apply bg-white;
     @apply drop-shadow;
+  }
+
+  .arrow {
+    width: 16px;
+    height: 16px;
+    @apply absolute bg-white;
+    @apply border-slate-300 border-t border-l;
   }
 </style>
--- client/src/views/TestingGrounds.vue
+++ client/src/views/TestingGrounds.vue
@@ -1,55 +1,20 @@
 <script setup>
+import LoremIpsum from '../components/LoremIpsum.vue'
 import MyButton from '../components/MyButton.vue'
 import MyPopover from '../components/MyPopover.vue'
-import LoremIpsum from '../components/LoremIpsum.vue'
-
-import { computed, onMounted, ref } from 'vue'
-import { arrow, autoPlacement, autoUpdate, flip, offset, shift, useFloating } from '@floating-ui/vue'
-const reference = ref(null),
-  floating = ref(null),
-  floatingArrowRef = ref(null),
-
-  { floatingStyles, middlewareData } = useFloating(reference, floating, {
-    middleware: [offset(12), autoPlacement(), arrow({ element: floatingArrowRef })],
-    whileElementsMounted: autoUpdate,
-  }),
-  style = computed(() => {
-    const side = middlewareData.value.offset?.placement.split('-')[0],
-      rotation = { top: -135, right: -45, bottom: 45, left: 135 }[side]
-    return {
-      transform: `rotate(${rotation}deg)`,
-      left: middlewareData.value.arrow?.x != null
-        ? `${middlewareData.value.arrow.x}px`
-        : `${floating.value?.offsetWidth * (side === 'left') - 9}px`,
-      top: middlewareData.value.arrow?.y != null
-        ? `${middlewareData.value.arrow.y}px`
-        : `${floating.value?.offsetHeight * (side === 'top') - 9}px`,
-    }
-  })
 </script>
 
 <template>
   <div class="flex w-screen h-screen items-center justify-center">
-    <main>
+    <main class="overflow-auto">
       <h1>Testing grounds</h1>
       <LoremIpsum />
-      <MyButton
-        ref="reference"
-        title="Button"
-        @click="console.log(middlewareData)"
-      />
-      <div
-        ref="floating"
-        class="floating"
-        :style="floatingStyles"
-      >
-        <p>Hello World!</p>
-        <div
-          ref="floatingArrowRef"
-          class="arrow"
-          :style="style"
-        />
-      </div>
+      <MyPopover>
+        <MyButton title="Button" />
+        <template #popover>
+          <p>Tooltip</p>
+        </template>
+      </MyPopover>
       <LoremIpsum />
     </main>
   </div>
@@ -58,19 +23,5 @@ const reference = ref(null),
 <style scoped>
   main {
     @apply w-1/2 h-1/2 bg-sky-200 p-4;
-  }
-
-  .floating {
-    @apply absolute top-0 left-0 px-6 py-3;
-    @apply border border-slate-300 rounded;
-    @apply bg-white;
-    @apply drop-shadow;
-  }
-
-  .arrow {
-    width: 16px;
-    height: 16px;
-    @apply absolute bg-white;
-    @apply border-slate-300 border-t border-l;
   }
 </style>
