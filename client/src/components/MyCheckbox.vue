85e553d31028f2029904975b7cf1117dd01f33ff
OrbitalOyster <orbitaloyster@gmail.com>
2024-12-01 20:28:43 +0300

    Checkbox rework

--- client/src/components/MyCheckbox.vue
+++ client/src/components/MyCheckbox.vue
@@ -1,4 +1,5 @@
 <script setup lang="ts">
+import { computed } from 'vue'
 import type { MyFormCheck } from '@stores/formStore.ts'
 import { useFormStore } from '@stores/formStore.ts'
 
@@ -8,49 +9,40 @@ const props = defineProps<{
     storeId: string
     checks?: MyFormCheck[]
   }>(),
-  store = useFormStore(props.storeId)
+  store = useFormStore(props.storeId),
+  toggle = () => {
+    console.log('Ok')
+    store.inputs[props.name] = !store.inputs[props.name]
+  },
+  isToggled = computed(() => store.inputs[props.name])
 
 store.checks[props.name] = props.checks ?? []
 store.inputs[props.name] = false
+
 </script>
 
 <template>
-  <label class="relative pl-8 select-none cursor-pointer"> {{ title }}
-    <input
-      v-model="store.inputs[props.name]"
-      class="absolute opacity-0 w-0 h-0"
-      type="checkbox"
-      :name
-    >
-    <span class="checkmark absolute inset-0 w-6 h-6 border-0 form-input transition" />
-  </label>
+  <input
+    v-model="store.inputs[props.name]"
+    class="hidden"
+    type="checkbox"
+    :name
+  >
+  <button type="button" class="foo relative form-input focusable transition" @click="toggle">
+    <div v-if="isToggled" class="block absolute inset-1.5 w-2.5 h-2.5 rounded bg-emerald-500"/>
+    <p class="pl-8 select-none cursor-pointer"> {{ title }} </p>
+  </button>
 </template>
 
 <style scoped>
 
-.checkmark::after {
-  @apply content-none;
-}
-
-/* On focus */
-input:focus ~ .checkmark {
-  @apply ring-4 ring-emerald-400;
-}
-
-/* On active */
-input:active ~ .checkmark {
-  @apply bg-emerald-700;
-}
-
-/* On checked - box */
-input:checked ~ .checkmark {
-  @apply bg-emerald-500;
-}
-
-/* On checked - mark */
-input:checked ~ .checkmark::after {
-  @apply block absolute inset-1.5 w-2.5 h-2.5 rounded bg-white;
+.foo {
+  @apply w-6 h-6 cursor-pointer;
   content: "";
+}
+
+.foo:active {
+  @apply bg-emerald-500;
 }
 
 </style>
