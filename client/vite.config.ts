cbb4ba84f67f2a0da1b75686a0d66cf513067534
OrbitalOyster <orbitaloyster@gmail.com>
2025-07-02 11:58:39 +0300

    Vite config dev. port

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,9 +1,12 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import vue from '@vitejs/plugin-vue'
+
+const env = loadEnv('development', process.cwd()),
+  port = Number(env['VITE_DEV_PORT'])
 
 export default defineConfig({
   server: {
-    port: 8080,
+    port,
   },
   plugins: [vue()],
   build: {
