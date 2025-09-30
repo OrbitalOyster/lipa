1f70864025481721330b07f54f2b91af887f9018
OrbitalOyster <orbitaloyster@gmail.com>
2025-09-30 14:45:39 +0300

    Testing grounds

--- client/src/views/TestView.vue
+++ client/src/views/TestView.vue
@@ -1,7 +1,11 @@
 <script setup lang="ts">
+import GooseButton from '#components/GooseButton.vue'
 </script>
 
 <template>
+  <h1>Testing grounds</h1>
+  <GooseButton title="Button 1" />
+  <GooseButton title="Button 2" />
   <div />
 </template>
 
