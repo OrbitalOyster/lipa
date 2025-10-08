ba5e74f717b9a3f9c06ad6bff1d0b62f1b60c96a
OrbitalOyster <orbitaloyster@gmail.com>
2025-10-08 12:18:59 +0300

    Uploading progress

--- api/src/xlsx/templates.ts
+++ api/src/xlsx/templates.ts
@@ -8,29 +8,25 @@ import { getPayload } from '../routes/cookies.ts'
 const attachmentName = 'attachment',
   uploadsFolder = '/tmp'
 
-async function parseXLSXFile(file: File) {
-  const buffer = await file.arrayBuffer(),
-    workbook = new ExcelJS.Workbook()
+async function parseXLSXFile(buffer: ArrayBuffer) {
+  const workbook = new ExcelJS.Workbook()
   await workbook.xlsx.load(buffer)
   const worksheets: XLSXWorksheet[] = []
-  workbook.eachSheet(
-    w => worksheets.push(new XLSXWorksheet(w)),
-  )
+  workbook.eachSheet(w => worksheets.push(new XLSXWorksheet(w)))
   return { worksheets }
 }
 
-export const validate = async (file: File) => {
+export const validate = async (buffer: ArrayBuffer) => {
   try {
-    const workbook = await parseXLSXFile(file)
+    const workbook = await parseXLSXFile(buffer)
     if (workbook.worksheets[0]) {
       const tables = workbook.worksheets[0].findTables()
-      return { ok: true, tables }
+      return { tables }
     }
-    return { ok: false, err: 'No worksheets' }
+    return { err: 'No worksheets' }
   }
   catch (err) {
-    console.log(err)
-    return { ok: false, err: 'Unable to parse' }
+    return { err: 'Unable to parse' }
   }
 }
 
@@ -43,15 +39,19 @@ export const upload = async (context: Context) => {
   if (!attachment || !(attachment instanceof File))
     return context.json('Missing attachment', 400)
   /* Convert, get hash */
-  const bytes = await attachment.bytes(),
-    hash = createHash('sha256').update(bytes).digest('hex'),
+  const bufferArray = await attachment.arrayBuffer(),
+    validation = await validate(bufferArray)
+  /* Unable to parse  validate */
+  if (validation.err)
+    return context.json(validation)
+  /* Save to tmp, return hash */
+  const buffer = Buffer.from(bufferArray),
+    hash = createHash('sha256').update(buffer).digest('hex'),
     time = new Date().valueOf(),
     fullFilename = `${uploadsFolder}/${userId}-${time}-${attachment.name}`,
     filenameHash = createHash('sha256').update(fullFilename).digest('hex')
   console.log(`Uploaded '${attachment.name}'\n\tsize ${attachment.size}\n\tuser ${userId}\n\ttime ${time}\n\thash ${hash}`)
-  await fs.writeFile(`${uploadsFolder}/${filenameHash}`, bytes)
+  await fs.writeFile(`${uploadsFolder}/${filenameHash}`, buffer)
   console.log('Saved to disk: ', filenameHash)
-
-  const validation = await validate(attachment)
-  return context.json(validation)
+  return context.json({ result: filenameHash })
 }
